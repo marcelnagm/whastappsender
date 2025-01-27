c7affcc0d3e74691317067ee3c5927d3d0fa432b
Marcel <marcel@marce>
2025-01-27 11:50:58 -0400

    trava  deativação para ativar

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -29,8 +29,8 @@ class RegisterController extends Controller
     {
         $user = User::create($request->validated());
 
-        auth()->login($user);
+        // auth()->login($user);
 
-        return redirect('/')->with('success', "Account successfully registered.");
+        return redirect('/')->with('success', "Conta Criada, entre em contato 5595981110695 para ativa-la.");
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="bg-light p-5 rounded">
+    @include('layouts.partials.messages')
         @auth
         <h1>Dashboard</h1>
         <p class="lead">Only authenticated users can access this section.</p>
@@ -9,8 +10,11 @@
         @endauth
 
         @guest
+                @include('layouts.partials.messages')
+
         <h1>Homepage</h1>
         <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
+        <p>Entre em contato 5595981110695 ou marcel.nagm@gmail.com para ativa-la</p>
         @endguest
     </div>
 @endsection
