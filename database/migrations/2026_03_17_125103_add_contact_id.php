9cca57653646e7e0c5e5c186e9cfa4719ac17ecf
Marcel <marcel@marce>
2026-03-17 09:34:29 -0400

    Adicionado lid

---
+++ database/migrations/2026_03_17_125103_add_contact_id.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('whatsapp_job', function (Blueprint $table) {            
+            $table->unsignedBigInteger('contact_id')->after('message_id');
+            $table->foreign('contact_id')->references('id')->on('contacts');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
